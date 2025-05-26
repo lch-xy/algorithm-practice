5d5c1fda926b0905e29ccbd54b8cffc2ad179aa8
L。 <421558569@qq.com>
2025-05-26 23:11:34 +0800

    add

--- src/main/java/org/lch/深度优先搜索丨广度优先搜索/Minesweeper_0529/Solution_1.java
+++ src/main/java/org/lch/深度优先搜索丨广度优先搜索/Minesweeper_0529/Solution_1.java
@@ -2,8 +2,53 @@ package org.lch.深度优先搜索丨广度优先搜索.Minesweeper_0529;
 
 public class Solution_1 {
 
-    public char[][] updateBoard(char[][] board, int[] click) {
+    final int[][] dirs = {
+            {-1, -1}, {-1, 0}, {-1, 1},
+            {0, -1}, {0, 1},
+            {1, -1}, {1, 0}, {1, 1},
+    };
 
-        return null;
+
+    // 游戏规则 点击雷就改成X
+    // 如果没有点击到雷就往外扩散，直到遇到数字
+    public char[][] updateBoard(char[][] board, int[] click) {
+        int row = click[0];
+        int col = click[1];
+        if (board[row][col] == 'M') {
+            board[row][col] = 'X';
+        } else {
+            bfs(board, row, col);
+        }
+        return board;
+    }
+
+    private void bfs(char[][] board, int row, int col) {
+        if (row < 0 || col < 0 || row >= board.length || col >= board[0].length || board[row][col] != 'E') {
+            return;
+        }
+        int count = countMines(board,row,col);
+        if (count > 0){
+            board[row][col] = (char) (count + '0');
+        }else{
+            board[row][col] = 'B';
+            for (int[] dir : dirs) {
+                bfs(board,row + dir[0],col+dir[1]);
+            }
+        }
+    }
+
+    private int countMines(char[][] board, int row, int col) {
+        int count = 0;
+        for (int[] dir : dirs) {
+            int newRow = row + dir[0];
+            int newCol = col + dir[1];
+            if (newRow < 0 || newCol < 0 || newRow >= board.length || newCol >= board[0].length ) {
+                continue;
+            }
+            if (board[newRow][newCol] == 'M'){
+                count++;
+            }
+        }
+        return count;
     }
 }
--- src/main/java/org/lch/深度优先搜索丨广度优先搜索/Word_Ladder_0127/Solution_2.java
+++ src/main/java/org/lch/深度优先搜索丨广度优先搜索/Word_Ladder_0127/Solution_2.java
@@ -5,8 +5,54 @@ import java.util.*;
 public class Solution_2 {
 
     // 双向bfs
+    // 使用两个 Set，一个从 beginWord 开始扩散，另一个从 endWord 开始。
+    // 每次从较小的集合扩展（减少分支数），如果两个集合有交集，说明找到了最短路径。
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> wordSet = new HashSet<>(wordList);
+        if (!wordSet.contains(endWord)) return 0;
 
+        Set<String> beginSet = new HashSet<>();
+        Set<String> endSet = new HashSet<>();
+        Set<String> visited = new HashSet<>();
+
+        beginSet.add(beginWord);
+        endSet.add(endWord);
+        int step = 1;
+
+        while (!beginSet.isEmpty() && !endSet.isEmpty()){
+            // 每次从小的集合开始扩散
+            if (beginSet.size() > endSet.size()){
+                Set<String> temp = beginSet;
+                beginSet = endSet;
+                endSet = temp;
+            }
+
+            Set<String> nextLevel = new HashSet<>();
+
+            for (String word : beginSet) {
+                char[] charArray = word.toCharArray();
+                for (int i = 0; i < word.length(); i++) {
+                    char old = charArray[i];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        if (c == old) continue;
+                        charArray[i] = c;
+                        String str = new String(charArray);
+                        if (endSet.contains(str)){
+                            return step + 1;
+                        }
+
+                        // 筛选并记录下一层可以扩展的合法单词
+                        if (wordSet.contains(str) && !visited.contains(str)){
+                            visited.add(str);
+                            nextLevel.add(str);
+                        }
+                    }
+                    charArray[i] = old;
+                }
+            }
+            beginSet = nextLevel;
+            step++;
+        }
         return 0;
     }
 
